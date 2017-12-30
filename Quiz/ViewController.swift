e6d8bdaf27a14229a0a4498ca9a860a74dd1bce3
UCode <ucode@UCode-MBA13-T03X.fios-router.home>
2017-12-30 15:11:06 -0800

    Initial commit

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by UCode on 12/30/17.
+//  Copyright © 2017 Jacob Luvisi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
